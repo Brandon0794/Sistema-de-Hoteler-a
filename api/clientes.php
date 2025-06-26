26c883cfe54a557e769040b7a85f9be3ea5ca750
Brandon0794 <165231072+Brandon0794@users.noreply.github.com>
2025-06-26 01:37:07 -0600

    commite de glenda

--- api/clientes.php
+++ api/clientes.php
@@ -1,9 +1,16 @@
 <?php
 // Permitir solicitudes desde cualquier origen (CORS)
-header("Access-Control-Allow-Origin: *");
+ // En desarrollo puedes usar '*'
+header("Access-Control-Allow-Origin: http://localhost:5173");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
-
+header("Content-Type: application/json");
+//header("Access-Control-Allow-Origin: http://localhost:5173");
+// Si la solicitud es de tipo OPTIONS (preflight), termina aquí
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 // Manejar solicitudes preflight (CORS - OPTIONS)
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
