0eaa6d34829ed6e7d642b0cf207c2dda47cb8949
Brandon0794 <165231072+Brandon0794@users.noreply.github.com>
2025-06-26 01:42:14 -0600

    ARREGLO DE LOS HEADERS
    SE arregla el pricipal problema que no había comunicacion con la API en los Headers de La Carpeta llamada API

--- api/usuario.php
+++ api/usuario.php
@@ -1,9 +1,9 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: http://localhost:5173");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
-// Encabezado para indicar que la respuesta es JSON
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Content-Type: application/json");
+require_once './../controller/DetalleReservacionAPIController.php';
 
 // Incluir el controlador API correspondiente
 require_once './../controller/UsuarioAPIController.php';
